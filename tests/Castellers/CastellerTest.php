bf13e0eb53c236cb6f4752a8936db117e1ca915a
Carlos Lombarte <lombartec@gmail.com>
2013-12-04 21:01:24 +0100

    - New test added for testing Casteller comparisons - Tests failing

--- tests/Castellers/CastellerTest.php
+++ tests/Castellers/CastellerTest.php
@@ -30,4 +30,48 @@ class CastellerTest extends \PHPUnit_Framework_TestCase
         $obj    = new Casteller( $height, $weight );
         $this->assertEquals( $weight, $obj->getWeight(), 'The weight returned by the object must be equal to ' . $weight );
     }
+
+    /**
+     * Data provider for testing the compare method.
+     *
+     * @return array
+     */
+    public function compareProvider()
+    {
+        return array(
+            'Equal Castellers' => array(
+                'first_casteller'   => new Casteller( 100, 100 ),
+                'second_casteller'  => new Casteller( 100, 100 ),
+                'expected_output'   => 0,
+                'message'           => 'When two castellers are equal 0 must be returned',
+            ),
+            'First Casteller is greater' => array(
+                'first_casteller'   => new Casteller( 200, 200 ),
+                'second_casteller'  => new Casteller( 100, 100 ),
+                'expected_output'   => 1,
+                'message'           => 'When the first casteller is greater 1 must be returned',
+            ),
+            'Second Casteller is greater' => array(
+                'first_casteller'   => new Casteller( 100, 100 ),
+                'second_casteller'  => new Casteller( 200, 200 ),
+                'expected_output'   => -1,
+                'message'           => 'When the second casteller is greater -1 must be returned',
+            ),
+        );
+    }
+
+    /**
+     * Tests that compare method can compare two Casteller instances.
+     *
+     * @dataProvider compareProvider
+     *
+     * @param Casteller $first_casteller    The first Casteller that will be compared with the second one.
+     * @param Casteller $second_casteller   The second Casteller that will be compared with the first one.
+     * @param integer   $expected_output    What compare method should return.
+     * @param string    $message            An error message to be shown in case of test fail.
+     */
+    public function testThatCompareCanCompareCastellersAsExpected( $first_casteller, $second_casteller, $expected_output, $message )
+    {
+        $this->assertEquals( $expected_output, $first_casteller->compare( $first_casteller, $second_casteller ), $message );
+    }
 }
