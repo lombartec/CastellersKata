e5df9f67e7964956aabbec8046f224662dcfeb0c
Carlos Lombarte <lombartec@gmail.com>
2013-12-06 00:17:49 +0100

    - Tests refactored and passing

--- tests/Castellers/CastellerSortTest.php
+++ tests/Castellers/CastellerSortTest.php
@@ -10,41 +10,44 @@ namespace Castellers;
 class CastellerSortTest extends \PHPUnit_Framework_TestCase
 {
     /**
-     * Tests that sortCastellerList method sorts Casteller list as expected.
+     * Tests that sortCastellerList method sorts Castellers correctly when the first casteller has less height and weight than the second.
      */
-    public function testThatSortCastellerListSortsCastellersCorrectly()
+    public function testThatSortCastellerListSortsWhenFirstCastellerIsLowerThanSecond()
     {
-        $casteller_a = $this->getMock( 'Castellers\Casteller', array( 'getHeight', 'getWeight' ), array(), 'Casteller', false );
-
-        $casteller_a->expects( $this->once() )
-            ->method( 'getHeight' )
-            ->will( $this->returnValue( 100 ) );
-
-        $casteller_a->expects( $this->once() )
-            ->method( 'getWeight' )
-            ->will( $this->returnValue( 100 ) );
-
-        $casteller_b = $this->getMock( 'Castellers\Casteller', array( 'getHeight', 'getWeight' ), array(), 'Casteller', false );
-
-        $casteller_b->expects( $this->once() )
-            ->method( 'getHeight' )
-            ->will( $this->returnValue( 200 ) );
-
-        $casteller_b->expects( $this->once() )
-            ->method( 'getWeight' )
-            ->will( $this->returnValue( 200 ) );
-
         $castellers = array(
-            $casteller_a,
-            $casteller_b,
+            $this->getCastellerMock( 100, 100 ),
+            $this->getCastellerMock( 200, 200 )
         );
 
-        $expected_castellers = array(
-            $casteller_b,
-            $casteller_a
+        $expected = array(
+            $castellers[1],
+            $castellers[0]
         );
 
         $obj = new CastellerSort();
-        $this->assertEquals( $expected_castellers, $obj->sortCastellersList( $castellers ), 'This method must return the expected ordered array' );
+        $this->assertEquals( $expected, $obj->sortCastellersList( $castellers ), 'The second casteller has to be the first after sorting' );
+    }
+
+    /**
+     * Creates a mock of the Casteller class with the expectations for the sorting method.
+     *
+     * @param integer $height The height returned by the mock when calling getHeight.
+     * @param integer $weight The weight returned by the mock when calling getWeight.
+     *
+     * @return \PHPUnit_Framework_MockObject_MockObject The mocked object.
+     */
+    private function getCastellerMock( $height, $weight )
+    {
+        $casteller_mock = $this->getMock( 'Castellers\Casteller', array( 'getHeight', 'getWeight' ), array(), 'Casteller', false );
+
+        $casteller_mock->expects( $this->once() )
+            ->method( 'getHeight' )
+            ->will( $this->returnValue( $height ) );
+
+        $casteller_mock->expects( $this->once() )
+            ->method( 'getWeight' )
+            ->will( $this->returnValue( $weight ) );
+
+        return $casteller_mock;
     }
 }
